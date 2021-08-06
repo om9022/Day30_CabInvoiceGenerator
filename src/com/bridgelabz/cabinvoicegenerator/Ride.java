7e7e6255fb50f6e134ca16e9cf5bfa675fd8ae2b
om9022 <omkarchikane607@gmail.com>
2021-08-06 23:45:07 +0530

    Added: Method to take multiple rides and calculate aggregate of total

---
+++ src/com/bridgelabz/cabinvoicegenerator/Ride.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class Ride {
+	public double distance;
+	public double time;
+	public Ride(double distance, double time) 
+	{
+		super();
+		this.distance = distance;
+		this.time = time;
+	}
+	@Override
+	public String toString() {
+		return "Ride [distance=" + distance + ", time=" + time + "]";
+	}
+	
+}
